a1e9d0e63fe300692ef9c58547795be4f884d957
Nicole Fontenot <nfontenot27@gmail.com>
2018-04-17 00:54:21 -0500

    Explore lifetime subtyping

--- rust-lang-book/lifetime.rs
+++ rust-lang-book/lifetime.rs
@@ -1,18 +1,23 @@
-struct Context<'a>(&'a str);
+struct Context<'s>(&'s str);
 
-struct Parser<'a> {
-    context: &'a Context<'a>,
+// 's: 'c - 's is guaranteed to outlive 'c
+struct Parser<'c, 's: 'c> {
+    context: &'c Context<'s>,
 }
 
-impl<'a> Parser<'a> {
-    fn parse(&self) -> Result<(), &str> {
+impl<'c, 's> Parser<'c, 's> {
+    fn parse(&self) -> Result<(), &'s str> {
         Err(&self.context.0[1..])
     }
 }
 
-//fn parse_context(context: Context) -> Result<(), &str> {
-//    Parser { context: &context }.parse()
-//}
+//the output of .parse will be a &str of context which was passed in.
+//Without different lifetimes for Parser and Context the output of
+// .parse becomes owned by the Parser that is local to this function
+// making this kind of information exchange impossible.
+fn parse_context(context: Context) -> Result<(), &str> {
+    Parser { context: &context }.parse()
+}
 
 fn main()
 {
@@ -24,9 +29,9 @@ fn main()
         println!("pear.parse: {}", s);
     }
 
-//    let fork = Context("This is another string to put in a context");
-//    if let Err(s) = parse_context(fork) {
-//        println!("parse_context(): {}", s);
-//    }
+    let fork = Context("This is another string to put in a context");
+    if let Err(s) = parse_context(fork) {
+        println!("parse_context(): {}", s);
+    }
 }
 
